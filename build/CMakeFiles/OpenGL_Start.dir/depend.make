bfd6877e409ce3f7fccf1423520f622e7f91884a
sorley.wang <sorley.wang@gmail.com>
2021-11-21 14:08:59 +0800

    add gitignore

--- build/CMakeFiles/OpenGL_Start.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for OpenGL_Start.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/w_sorley/Workspace/C:C++/OpenGL
-# Build directory: /Users/w_sorley/Workspace/C:C++/OpenGL/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
