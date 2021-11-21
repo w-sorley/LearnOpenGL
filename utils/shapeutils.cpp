4619d2c0997ab0bf1894965d390e84b793f01916
sorley.wang <sorley.wang@gmail.com>
2021-11-21 14:11:03 +0800

    draw rectangle using element buffer

--- utils/shapeutils.cpp
+++ utils/shapeutils.cpp
@@ -21,10 +21,17 @@ unsigned int VAO;
 
 int ShapeUtils::init(){
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f,  -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f   
+        0.5f,  0.5f, 0.0f, 
+        0.5f,  -0.5f,  0.0f,
+       -0.5f,  -0.5f,  0.0f,
+       -0.5f,   0.5f,  0.0f    
     };
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);
@@ -34,11 +41,19 @@ int ShapeUtils::init(){
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
+  
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     program = GLUtils::CreateProgram(VERTEX_SHADER, FRAGMENT_SHADER);
 }
@@ -46,6 +61,8 @@ int ShapeUtils::DrawTriangle(){
 
     glUseProgram(program);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    // glBindVertexArray(0);
     return 0;
 }
