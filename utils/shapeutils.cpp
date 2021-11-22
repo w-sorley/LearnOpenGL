31d694a9e00be169fbece79f6144393145acfcd3
sorley.wang <sorley.wang@gmail.com>
2021-11-22 13:05:59 +0800

    transfer data in shader

--- utils/shapeutils.cpp
+++ utils/shapeutils.cpp
@@ -3,16 +3,19 @@
 
 const char *VERTEX_SHADER = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
+        "out vec4 vertexColor;\n"
         "void main()\n"
         "{\n"
         "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "    vertexColor = vec4(0.0, 0.5, 0.0, 0.0);\n"
         "}\0";
         
 const char *FRAGMENT_SHADER = "#version 330 core\n"
         "out vec4 FragColor;\n"
+        "in vec4 vertexColor;"
         "void main()\n"
         "{\n"
-        "    FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+        "    FragColor = vertexColor;\n"
         "}\0";
 
 GLuint ShapeUtils::program = 0;
