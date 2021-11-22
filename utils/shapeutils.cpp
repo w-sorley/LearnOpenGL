fe11c9bdd5f7b5e810db5be28caaaa02a456d51b
sorley.wang <sorley.wang@gmail.com>
2021-11-22 13:35:45 +0800

    transfer postion and color using VBO

--- utils/shapeutils.cpp
+++ utils/shapeutils.cpp
@@ -3,19 +3,20 @@
 
 const char *VERTEX_SHADER = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
-        "out vec4 vertexColor;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "out vec3 vertexColor;\n"
         "void main()\n"
         "{\n"
         "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "    vertexColor = vec4(0.0, 0.5, 0.0, 0.0);\n"
+        "    vertexColor = aColor;\n"
         "}\0";
         
 const char *FRAGMENT_SHADER = "#version 330 core\n"
         "out vec4 FragColor;\n"
-        "uniform vec4 ourColor;\n"
+        "in vec3 vertexColor;\n"
         "void main()\n"
         "{\n"
-        "    FragColor = ourColor;\n"
+        "    FragColor = vec4(vertexColor, 1.0);\n"
         "}\0";
 
 GLuint ShapeUtils::program = 0;
@@ -24,10 +25,9 @@ unsigned int VAO;
 
 int ShapeUtils::init(){
     float vertices[] = {
-        0.5f,  0.5f, 0.0f, 
-        0.5f,  -0.5f,  0.0f,
-       -0.5f,  -0.5f,  0.0f,
-       -0.5f,   0.5f,  0.0f    
+        0.5f,   -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f,   0.5f,  0.0f, 0.0f, 0.0f, 1.0f,   
     };
 
     unsigned int indices[] = {
@@ -44,19 +44,21 @@ int ShapeUtils::init(){
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // unsigned int EBO;
+    // glGenBuffers(1, &EBO);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
   
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     program = GLUtils::CreateProgram(VERTEX_SHADER, FRAGMENT_SHADER);
 }
@@ -68,8 +70,8 @@ int ShapeUtils::DrawTriangle(){
     glUseProgram(program);
     glUniform4f(vertexColorLocation, 0.0, 0.0, blueValue, 0.0);
     glBindVertexArray(VAO);
-    // glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     // glBindVertexArray(0);
     return 0;
 }
