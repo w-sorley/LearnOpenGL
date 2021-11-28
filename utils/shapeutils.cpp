faa1631cbaa1882406260d813529486c7a8e6942
sorley.wang <sorley.wang@gmail.com>
2021-11-28 22:44:00 +0800

    coordinate system : multiple 3D box

--- utils/shapeutils.cpp
+++ utils/shapeutils.cpp
@@ -80,9 +80,7 @@ int ShapeUtils::DrawTriangle(){
     m_shader->setInt("texture1", 0);
     m_shader->setInt("texture2", 1);
 
-    glm::mat4 model = glm::mat4(1.0f); 
-    model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f)); 
-    glUniformMatrix4fv(glGetUniformLocation(m_shader->ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
 
     m_shader->use();
     glBindVertexArray(VAO);
@@ -90,7 +88,30 @@ int ShapeUtils::DrawTriangle(){
     glBindTexture(GL_TEXTURE_2D, texture1);
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, texture2);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
+    for(int i = 0; i < 10; i++){
+        glm::mat4 model = glm::mat4(1.0f); 
+        model = glm::translate(model, cubePositions[i] / 4.0f); 
+        model = glm::scale(model, glm::vec3(0.2f, 0.2f, .2f));
+        float angle = 20.0f * i; 
+        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+        glUniformMatrix4fv(glGetUniformLocation(m_shader->ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
+
     // glBindVertexArray(0);
 
     return 0;
