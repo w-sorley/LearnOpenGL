68c6c522572c39642aafbcdf6ec0b162f43286b0
sorley.wang <sorley.wang@gmail.com>
2021-11-28 22:16:50 +0800

    coordinate system : basic transform

--- utils/shapeutils.cpp
+++ utils/shapeutils.cpp
@@ -56,11 +56,18 @@ int ShapeUtils::DrawTriangle(){
     m_shader->setInt("texture1", 0);
     m_shader->setInt("texture2", 1);
 
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0));
-    trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+    glm::mat4 model = glm::mat4(1.0f); 
+    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f)); // 绕x轴旋转-55
 
-    glUniformMatrix4fv(glGetUniformLocation(m_shader->ID, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
+    glm::mat4 view = glm::mat4(1.0f);
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // z轴负方向移动3
+
+    glm::mat4 projection  = glm::mat4(1.0f);
+    projection = glm::perspective(glm::radians(45.0f), 7.0f/8.0f, 0.1f, 100.0f);
+
+    glUniformMatrix4fv(glGetUniformLocation(m_shader->ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+    glUniformMatrix4fv(glGetUniformLocation(m_shader->ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(glGetUniformLocation(m_shader->ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
     m_shader->use();
     glUniform4f(vertexColorLocation, 0.0, 0.0, blueValue, 0.0);
     glBindVertexArray(VAO);
@@ -72,10 +79,5 @@ int ShapeUtils::DrawTriangle(){
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     // glBindVertexArray(0);
 
-    glm::mat4 trans2 = glm::mat4(1.0f);
-    trans2 = glm::translate(trans2, glm::vec3(-0.5f, 0.5f, 0.0));
-    trans2 = glm::scale(trans2, glm::vec3(sin((float)glfwGetTime()), 1.0f, 1.0f));
-    glUniformMatrix4fv(glGetUniformLocation(m_shader->ID, "transform"), 1, GL_FALSE, glm::value_ptr(trans2));
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     return 0;
 }
